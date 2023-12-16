f005b73b020be256d1a42ffb7e55e495e64d4366
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-16 04:48:17 +0300

    fetching todos from pinia store

---
+++ src/components/headerSection.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex gap-5 justify-items-center justify-center">
+      <img class="h-10" src="../assets/img.png" alt="Pinia Logo">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Pinia Todos</h1>
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/todoCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {defineProps} from 'vue'
+
+const props = defineProps({
+  todo: Object
+})
+
+
+</script>
+
+<template>
+  <div class=" my-2 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class="md:flex">
+<!--      <div class="md:flex-shrink-0">-->
+<!--        <img class="h-48 w-full object-cover md:w-48" src="../assets/img.png" alt="Man looking at item at a store">-->
+<!--      </div>-->
+      <div class="p-8">
+        <div>
+          {{todo.task}}
+        </div>
+        <p class="mt-2 text-gray-500">
+          Getting a new business off the ground is a lot of hard work.
+          Here are five ideas you can use to find your first customers.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
